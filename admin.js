b26403b62dd9a4cae8222ee8a2802eb107d2370a
Marcoantonio <marcoantoniohernandez2321@gmail.com>
2026-07-07 21:30:05 -0400

    Delete admin.js

--- admin.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("logout-btn").addEventListener("click", () => {
-    document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.location.href = "/";
-});
